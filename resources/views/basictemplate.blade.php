295c6b32f11459c88546e712a74decf62429c83e
M Ahmed Mushtaq <mmushtaq.bese19seecs@seecs.edu.pk>
2020-04-11 22:22:14 +0500

    Todo App In Laravel

---
+++ resources/views/basictemplate.blade.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>The HTML5 Herald</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+ 
+
+</head>
+
+<body>
+ 
+ 
+
+</body>
+</html>
